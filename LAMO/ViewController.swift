1c9622389d00a06a7ede07968b7c3f67e7f0d4a9
Chau Tung Lam <nctl144@gmail.com>
2017-07-03 14:24:57 -0700

    Initial Commit

---
+++ LAMO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LAMO
+//
+//  Created by Nguyễn Lâm on 7/3/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
